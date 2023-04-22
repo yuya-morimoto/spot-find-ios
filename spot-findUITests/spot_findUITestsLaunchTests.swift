f5b0c9bc9a06a8eca528f4003aa27f86972a774a
yuya-morimoto <gyu2243dev@gmail.com>
2023-04-22 15:50:25 +0900

    feat: first commit

---
+++ spot-findUITests/spot_findUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  spot_findUITestsLaunchTests.swift
+//  spot-findUITests
+//
+//  Created by Yuya Morimoto on R 5/04/22.
+//
+
+import XCTest
+
+final class spot_findUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
