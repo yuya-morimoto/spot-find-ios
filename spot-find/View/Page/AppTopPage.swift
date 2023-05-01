ba9665a3a92d77629f9ced14a2daac3065e21f3e
yuya-morimoto <gyu2243dev@gmail.com>
2023-05-01 17:24:42 +0900

    feat: sample top app page #17

--- spot-find/View/Page/AppTopPage.swift
+++ spot-find/View/Page/AppTopPage.swift
@@ -6,15 +6,31 @@
 //
 
 import ComposableArchitecture
+import MapKit
 import SwiftUI
 
 struct AppTopPage: View {
     let store: AppStore
 
+    @State private var region = MKCoordinateRegion(
+        // サンプルに東京駅の座標指定
+        center: CLLocationCoordinate2D(latitude: 35.6809591,
+                                       longitude: 139.7673068),
+        latitudinalMeters: 750,
+        longitudinalMeters: 750
+    )
+
     var body: some View {
-        WithViewStore(self.store, observe: \.auth) { viewStore in
-            Button("sign out") {
-                viewStore.send(.auth(.signOut))
+        WithViewStore(self.store) { viewStore in
+            ZStack {
+                Map(coordinateRegion: $region)
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    Button("ログアウトする") {
+                        viewStore.send(.auth(.signOut))
+                    }.buttonStyle(CapsuleButtonStyle(backgroundColor: .Primary, foregroundColor: .PrimaryContrast))
+                }.padding()
             }
         }
     }
